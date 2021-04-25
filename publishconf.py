1c7e07900ee92ca27ac4a359f29b39470a1534dd
Emmanuele Somma <exedre@gmail.com>
2021-04-25 12:09:15 +0200

    good-site

--- publishconf.py
+++ publishconf.py
@@ -11,7 +11,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'https://old.pir8.pw'
+SITEURL = 'https://pir8.pw'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
